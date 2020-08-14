dbec362e9700b021d4cba697d94eb81a2d0d6624
Andrzej <andrzej.przybysz01@gmail.com>
2020-08-14 20:56:12 +0200

    dockerize api and mongo db

--- config/mail_config.js
+++ config/mail_config.js
@@ -1,7 +1,9 @@
 module.exports = {
     host: process.env.MAILER_HOST,
     port: process.env.MAILER_PORT,
-    secure: process.env.SECURE,
+    secure: process.env.MAILER_SECURE,
+    pool: true,
+    debug: process.env.DEBUG,
     auth: {
         user: process.env.MAILER_USER,
         pass: process.env.MAILER_PASSWORD
